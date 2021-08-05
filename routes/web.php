50f626a25c6f5c1358e4e558388366b997cfbd2d
Russkazkin <ruslan@skazkin.su>
2021-08-05 11:12:49 +0300

    LSN-2: Add StaticPageController

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,6 @@
 <?php
 
+use App\Http\Controllers\StaticPageController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/about', function () {
-    return view('static.about');
-});
-Route::get('/users', function () {
-    return view('static.user');
-});
-Route::get('/news', function () {
-    return view('static.news');
-});
+Route::get('/', [StaticPageController::class, 'welcome']);
