49788d7dac34dbc1e206520ce8a85abc917d2260
Russkazkin <ruslan@skazkin.su>
2021-08-07 15:24:06 +0300

    LSN-3: Add admin layout template

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\NewsController;
 use App\Http\Controllers\StaticPageController;
@@ -9,3 +10,7 @@ Route::get('/', [StaticPageController::class, 'welcome']);
 
 Route::resource('category', CategoryController::class);
 Route::resource('news', NewsController::class);
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+    Route::get('/', AdminController::class)->name('index');
+});
