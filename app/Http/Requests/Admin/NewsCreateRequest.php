f26600aa692db92935bbcd19ba791835555f6a6c
Russkazkin <ruslan@skazkin.su>
2021-08-23 06:18:04 +0300

    LSN-7: Add news create and update validation

--- app/Http/Requests/Admin/NewsCreateRequest.php
+++ app/Http/Requests/Admin/NewsCreateRequest.php
@@ -11,9 +11,9 @@ class NewsCreateRequest extends FormRequest
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -21,10 +21,13 @@ class NewsCreateRequest extends FormRequest
      *
      * @return array
      */
-    public function rules()
+    public function rules(): array
     {
         return [
-            //
+            'title' => 'required|string',
+            'content' => 'required|string',
+            'categories' => 'required|array|exists:categories,id',
+            'image' => 'sometimes|string'
         ];
     }
 }
