1cf25871fa62579b03402f41be34346761a63158
Russkazkin <ruslan@skazkin.su>
2021-08-25 05:50:36 +0300

    LSN-8: Add DashboardController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\NewsController;
 use App\Http\Controllers\Admin\NewsController as AdminNewsController;
@@ -17,10 +18,7 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => ['auth']], 
     Route::get('/', AdminController::class)->name('index');
     Route::resource('news', AdminNewsController::class);
     Route::resource('category', AdminCategoryController::class);
+    Route::resource('dashboard', DashboardController::class);
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
